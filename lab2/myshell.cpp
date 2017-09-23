f2ff63f19baefd936bf5ecc57f9e51f496d70a48
J888 <0xjohn@gmail.com>
2017-09-23 00:01:19 -0400

    continuing work on project 2

--- lab2/myshell.cpp
+++ lab2/myshell.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(void)
 {
 	
+string the_line;
 
+int argc;
 
+char* argv[100];
+
+while(true)
+{
+	cout << "J-Shell> ";
+
+	getline(cin, the_line); 
+
+	cout << "Command entered was: " << the_line;
+	cout << endl;
+
+	if((the_line == "exit") && (!cin.eof()))
+	{
+		exit(0);
+	}
+}
 
 }
