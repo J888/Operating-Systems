77838fa0ff2308059adb61c5ac7b1f1939d6b7c4
J888 <0xjohnh@gmail.com>
2017-10-03 19:47:37 -0400

    now have a way to convert vector

--- lab2/learning.cpp
+++ lab2/learning.cpp
@@ -8,61 +8,86 @@
 
 using namespace std;
 
-#include "evaluater.h"
 
 
 
-int my_fork(int shouldwait)
-{
 
-	pid_t pid;
+// int my_fork(int shouldwait, const char* ch[])
+// {
 
-	pid = fork();
+// 	pid_t pid;
 
-	if(pid == -1)
-	{
-		cout << "error" << endl;
-		exit(-1);
-	}
-	else if(pid == 0)
-	{
-		//child
-		sleep(10);
-		cout << "I am child" << endl;
+// 	pid = fork();
 
-		exit(0);
-	}
+// 	if(pid == -1)
+// 	{
+// 		cout << "error" << endl;
+// 		exit(-1);
+// 	}
+// 	else if(pid == 0)
+// 	{
+// 		//child
+		
+// 		execv("./hey", ch);
 
-	else
-	{
-		if(shouldwait)
-		{
-			wait(NULL);
-		}
+// 		exit(0);
+// 	}
+
+// 	else
+// 	{
+// 		if(shouldwait)
+// 		{
+// 			wait(NULL);
+// 		}
 
 		
 
 
+// 	}
+
+// 	return 1;
+
+
+// }
+
+
+/*converts vector of strings into an array of const char* */
+const char** convert_vector(vector<string> s)
+{
+
+	int the_size = s.size();
+
+	const char **argv = new const char*[the_size+2];
+
+	argv[0] = s[0].c_str();
+
+	for(int i = 0; i < the_size+1; i++)
+	{
+		argv[i+1] = s[i].c_str();
 	}
 
-	return 1;
+	argv[the_size+1] = NULL;
 
+	return argv;
 
 }
-
 
 
 
 int main(void)
 {
 
- 	string hey = "hey";
 
- 	string s = "this is a test string";
+ 	vector<string> strs = {"./program", "arg1", "arg2", "arg3"};
 
- 	vector<string> v = parse_line(s);
+ 	const char** commands = convert_vector(strs);
 
- 	cout << v[0] << endl << v[1];
+ 	cout << "1 is: " << commands[0] << endl;
+ 	cout << "2 is: " << commands[1] << endl;
+ 	cout << "3 is: " << commands[2] << endl;
+	cout << "4 is: " << commands[3] << endl;
+
+
 
 
 
